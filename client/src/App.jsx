f127b2f7b2081442be7f65d1059258e47e8765a1
Nadir Hossain <nadirhossain336@gmail.com>
2024-02-11 17:06:29 +0600

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,7 @@
 import { Route, Routes  } from 'react-router-dom';
 import Home from './pages/Home';
 import Room from './pages/Room';
-import GroupHome from './pages/Group/GroupHome';
-import TakeUser from './pages/Group/TakeUser';
-import Stream from './pages/Group/Stream';
+
 
 
 function App() {
@@ -12,9 +10,6 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/room/:roomId' element={<Room/>}></Route>
-        <Route path='/meeting' element={<GroupHome/>}></Route>
-        <Route path='/meeting/:meetingID' element={<TakeUser/>}></Route>
-        <Route path='/stream/:meetingID' element={<Stream/>}></Route>
       </Routes>
     </div>
   )
